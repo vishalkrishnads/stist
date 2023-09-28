76f1a17bcf7eaa36063e13848967f71719107068
vishalkrishnads <321vishalds@gmail.com>
2023-09-28 13:53:29 +0530

    Changed credits

--- app/components/Header.js
+++ app/components/Header.js
@@ -6,9 +6,9 @@ export default function Header() {
     const router = useRouter()
     
     return <div className='header'>
-    <div onClick={() => router.push('/')}>
+    <div className='link' onClick={() => router.push('/')}>
       <h1><u>Ruchi</u>kootu</h1>
-      <h4>STIST x <a>FOSS United</a></h4>
+      <h4>ICFOSS x <a>FOSS United</a></h4>
     </div>
   </div>
 }
